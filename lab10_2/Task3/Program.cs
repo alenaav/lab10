d253ad36bce1d53b97a3d0a9dad5fa2b183c3a19
alenaav <144984409+alenaav@users.noreply.github.com>
2024-04-03 00:38:18 +0500

    Add files via upload

---
+++ lab10_2/Task3/Program.cs
@@ -0,0 +1,427 @@
+﻿using System.Diagnostics;
+using AutomobileLibrary;
+using lab10;
+
+namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestCollections collections = new TestCollections();
+
+        Stopwatch stopwatch = new();
+
+        Car first = (Car)collections.First.Clone();
+        Car mid = (Car)collections.Mid.Clone();
+        Car last = (Car)collections.Last.Clone();
+        Car nonexistent = (Car)collections.Nonexistent.Clone();
+
+        const int iterations = 1000;
+        bool isFound = false;
+        long time;
+
+        Console.WriteLine("   Поиск для LinkedList<TValue>:\n");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection1.Contains(first);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection1.Contains(mid);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection1.Contains(last);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection1.Contains(nonexistent);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+
+        Console.WriteLine("\n\n   Поиск для LinkedList<string>:\n");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection2.Contains(first.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection2.Contains(mid.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection2.Contains(last.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection2.Contains(nonexistent.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+
+
+        Console.WriteLine("\n\n   Поиск для Dictionary<TKey, TValue>:\n");
+
+
+        Console.WriteLine("ПО КЛЮЧУ:");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsKey(first.GetBase);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsKey(mid.GetBase);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsKey(last.GetBase);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsKey(nonexistent.GetBase);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+
+        Console.WriteLine("\nПО ЗНАЧЕНИЮ:");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsValue(first);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsValue(mid);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsValue(last);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection3.ContainsValue(nonexistent);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+
+        Console.WriteLine("\n\n   Поиск для Dictionary<string,TValue>:\n");
+
+        Console.WriteLine("ПО КЛЮЧУ:");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsKey(first.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsKey(mid.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsKey(last.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsKey(nonexistent.GetBase.ToString());
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+
+        Console.WriteLine("\nПО ЗНАЧЕНИЮ:");
+
+        // поиск первого элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsValue(first);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Первый элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Первый элемент не найден");
+
+        // поиск среднего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsValue(mid);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Средний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Средний элемент не найден");
+
+        // поиск последнего элемента
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsValue(last);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Последний элемент найден за {time} тиков");
+        else
+            Console.WriteLine($"Последний элемент не найден");
+
+        // поиск элемента вне коллекции
+        time = 0;
+        for (int i = 0; i < iterations; i++)
+        {
+            stopwatch.Start();
+            isFound = collections.Collection4.ContainsValue(nonexistent);
+            stopwatch.Stop();
+            time += stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+        }
+        time /= iterations;
+        if (isFound)
+            Console.WriteLine($"Элемент вне коллекции найден за {time} тиков");
+        else
+            Console.WriteLine($"Элемент вне коллекции не найден, затрачено времени: {time} тиков");
+    }
+}
+
---
+++ lab10_2/Task3/TestCollections.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AutomobileLibrary;
+namespace lab10
+{
+	public class TestCollections
+	{
+        LinkedList<Car> collection1 = new();
+        LinkedList<string> collection2 = new();
+
+        Dictionary<Automobile, Car> collection3 = new();
+        Dictionary<string, Car> collection4 = new();
+
+
+        const int collectionsLength = 1000;
+
+        const int indexFirst = 0;
+        const int indexMid = collectionsLength / 2 - 1;
+        const int indexLast = collectionsLength - 1;
+
+        Car first, mid, last;
+        Car nonexistent = new("не существует", 2000, "не существует", 0, 0, new IdNumber(0), 0, 0);
+
+        public TestCollections()
+        {
+            for (int i = 0; i < collectionsLength; i++)
+            {
+                Car c = new();
+                c.RandomInit();
+                c.Brand += i.ToString();
+                collection1.AddLast(c);
+                collection2.AddLast(c.GetBase.ToString());
+                collection3.Add(c.GetBase, c);
+                collection4.Add(c.GetBase.ToString(), c);
+                if (i == indexFirst)
+                    first = c;
+                if (i == indexMid)
+                    mid = c;
+                if (i == indexLast)
+                    last = c;
+            }
+        }
+
+        public LinkedList<Car> Collection1 { get => this.collection1; }   // автосвойство для получения коллекции 1
+        public LinkedList<string> Collection2 { get => this.collection2; }    // автосвойство для получения коллекции 2
+        public Dictionary<Automobile, Car> Collection3 { get => this.collection3; }                     // автосвойство для получения коллекции 3
+        public Dictionary<string, Car> Collection4 { get => this.collection4; }                         // автосвойство для получения коллекции 4
+
+        public Car First { get => this.first; }   // автосвойство для получения первого элемента
+        public Car Mid { get => this.mid; }       // автосвойство для получения центрального элемента
+        public Car Last { get => this.last; }     // автосвойство для получения последнего элемента
+        public Car Nonexistent { get => this.nonexistent; }
+
+       
+	}
+}
+
